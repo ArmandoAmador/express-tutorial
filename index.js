1586799dc24215de0e8058085efe0a01feba4972
armandoamador <arm.amador@gmail.com>
2019-01-16 21:50:17 -0500

    Add 404 page

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.get('/blog', function(req, res) {
   res.send('Blog Page');
 })
 
+// 404 Page
+app.use(function (req, res, next) {
+  res.status(404);
+  res.send("Oh Oh, you've reached a page not found");
+});
+
 app.listen(3000, function() {
   console.log('Server is running at http://localhost:3000');
 });
