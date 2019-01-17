15033a56f4504fa491d11ca08e02a45bf654d6c4
armandoamador <arm.amador@gmail.com>
2019-01-16 20:57:35 -0500

    Setup new routes for About and Blog pages

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 
+// Home Page
 app.get('/', function(req, res) {
   res.send('Hello World');
 });
+
+// About Page
+app.get('/about', function(req, res) {
+  res.send('About Page');
+})
+
+// Blog Page
+app.get('/blog', function(req, res) {
+  res.send('Blog Page');
+})
 
 app.listen(3000, function() {
   console.log('Server is running at http://localhost:3000');
